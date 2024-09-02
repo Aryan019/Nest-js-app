50903c3cdab62c4d870544a97e3978ef9d8b4bfd
Aryan019 <aryan19v@gmail.com>
2024-09-02 17:04:13 +0530

    Initial commit 2 nest

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,19 +1,54 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from 'src/app.service';
+import { CreateTodo } from './todo.dto';
 
 @Controller('todo')
 export class TodoController {
 
 constructor(private appservice : AppService){}
 
+private todos=[]
+
 @Get("/create")
 createTodo(){
     return this.appservice.createtodo()
 }
 
+// CreateTodo is a validation pipeline here 
+// and goes through checks the validation pipeline here is with the name 
+// todo.dto.ts
+
+// and a middleware is also there in the main.ts file 
 @Post("/create/post")
-createPost(@Body() data){
-    return data.name
+createPost(@Body() data: CreateTodo){
+    // return data
+
+    // Saving the data to the above array
+    const item = {
+
+        id: new Date().getTime(),
+        // Spreading the data
+        ...data,
+
+        createdAt: new Date().toLocaleString()
+    }
+
+    this.todos.push(item)
+
+    return {
+        msg : "todo is created"
+    }
+}
+
+@Get('/get')
+getAllTodos(){ 
+
+    return {
+        todos : this.todos,
+        length: this.todos.length,
+        msg : "All the movies are listed above "
+    }
+
 }
 
 
--- src/todo/todo.dto.ts
+++ src/todo/todo.dto.ts
@@ -1,3 +1,14 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+// Applying the validations to the validation pipline
 export class CreateTodo{
-    
+
+    @IsNotEmpty({message : 'Name chahiye bhai '})
+    title : string
+
+
+    // Applying in the email validations here 
+    // @IsEmail({})
+    @IsNotEmpty()
+    desc : string
 }
